5f41cbbb0d3c69d2dc7a642b54a436e77e38b890
Dimitrios-Nikolaos Sinogeorgos <dnsinogeorgos@protonmail.ch>
2024-06-13 01:07:50 +0300

    more work done, no strings to be found

--- internal/ccmasker/process.go
+++ internal/ccmasker/process.go
@@ -2,10 +2,10 @@ package ccmasker
 
 import (
 	"bytes"
+	"errors"
 	"io"
 	"log"
 	"regexp"
-	"strconv"
 
 	"github.com/mailru/easyjson/jwriter"
 	"github.com/theplant/luhn"
@@ -13,7 +13,7 @@ import (
 
 //easyjson:json
 type Message struct {
-	Msg string `json:"msg,nocopy"`
+	Msg []byte
 }
 
 // ProcessMessage filters the message through regexp filters and returns appropriate response for rsyslog
@@ -32,7 +32,7 @@ func ProcessMessage(out io.Writer, message []byte, filters []filterGroup, numFil
 					for _, match := range matchStrings {
 						// Prepare string for Luhn check
 						cleanMatch := numFilter.ReplaceAll(match, []byte{})
-						cleanInt, err := strconv.Atoi(string(cleanMatch))
+						cleanInt, err := parseIntFromBytes(cleanMatch)
 						if err != nil {
 							return err
 						}
@@ -59,7 +59,7 @@ func ProcessMessage(out io.Writer, message []byte, filters []filterGroup, numFil
 	// If PAN data is found, wrap to JSON and return
 	jw := jwriter.Writer{}
 	message = bytes.TrimSuffix(message, []byte{'\n'})
-	jsonMessage := Message{Msg: string(message)}
+	jsonMessage := Message{Msg: message}
 	jsonMessage.MarshalEasyJSON(&jw)
 	_, err := jw.DumpTo(out)
 	if err != nil {
@@ -71,4 +71,69 @@ func ProcessMessage(out io.Writer, message []byte, filters []filterGroup, numFil
 	}
 
 	return nil
+}
+
+// parseIntFromBytes is the equivalent of strconv.Atoi but for byteslices
+func parseIntFromBytes(b []byte) (int, error) {
+	if len(b) == 0 {
+		return 0, errors.New("empty byte slice")
+	}
+
+	var num int
+	var sign = 1
+
+	start := 0
+	if b[0] == '-' {
+		sign = -1
+		start = 1
+	}
+
+	for i := start; i < len(b); i++ {
+		if b[i] < '0' || b[i] > '9' {
+			return 0, errors.New("invalid byte slice")
+		}
+		num = num*10 + int(b[i]-'0')
+	}
+
+	return sign * num, nil
+}
+
+// MarshalEasyJSON supports easyjson.Marshaler interface
+func (in Message) MarshalEasyJSON(out *jwriter.Writer) {
+	out.RawByte('{')
+
+	const prefix string = ",\"Msg\":"
+	out.RawString(prefix[1:])
+
+	// Manually handle byte slice as a JSON string
+	out.RawByte('"')
+	for _, b := range in.Msg {
+		switch b {
+		case '"':
+			out.RawString(`\"`)
+		case '\\':
+			out.RawString(`\\`)
+		case '\b':
+			out.RawString(`\b`)
+		case '\f':
+			out.RawString(`\f`)
+		case '\n':
+			out.RawString(`\n`)
+		case '\r':
+			out.RawString(`\r`)
+		case '\t':
+			out.RawString(`\t`)
+		default:
+			if b < 0x20 {
+				out.RawString(`\u00`)
+				out.RawByte("0123456789abcdef"[b>>4])
+				out.RawByte("0123456789abcdef"[b&0xF])
+			} else {
+				out.RawByte(b)
+			}
+		}
+	}
+	out.RawByte('"')
+
+	out.RawByte('}')
 }
